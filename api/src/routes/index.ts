df08c2e7f31efb057fb6b64c77aeffc38a702d4a
Joniekesh <okorojohndarlington@gmail.com>
2026-01-08 01:54:22 +0100

    commit

--- api/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sayHello } from "../controllers/helloController";
-
-const router: Router = Router();
-
-router.get("/", sayHello);
-
-export default router;
