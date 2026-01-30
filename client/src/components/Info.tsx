242c5bc050a2adb2fa772c809e80e2b2f6fba8d2
Joniekesh <okorojohndarlington@gmail.com>
2026-01-30 07:40:17 +0100

    commit

--- client/src/components/Info.tsx
+++ client/src/components/Info.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 const Info = () => {
   return (
-    <div className="flex flex-col gap-8 h-max ">
+    <div className="flex flex-col gap-8 h-max -mt-30">
       <p className="text-center text-3xl">
         <b>ABA Power</b> - We distribute electiricity <br /> through {""}
         <span className="text-amber-500">innovation</span> and with ultimate{" "}
